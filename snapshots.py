e321119d108828252ec49bce8d62f040be2bc328
Oliver Crocker <s2521629@ed.ac.uk>
2026-09-23 17:31:29 +0100

    graphing the positions of the particles

--- snapshots.py
+++ snapshots.py
@@ -1,12 +1,22 @@
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
 
-path = "/disk12/legacy/GVD_C700_l100n256_SLEGAC/dm_gadget/data/snapdir_005"
+base_path = Path("/disk12/legacy/GVD_C700_l100n256_SLEGAC/dm_gadget/data")
+snapshot_numbers = [5, 6, 7, 8, 9]  # 5 snapshots
+n_files_per_snapshot = 4
 
-positions = []
+fig, ax = plt.subplots(figsize=(8, 8))
 
-for i in range(4):
-    file = f"{path}/snapshot_005.{i}.hdf5"
+for snap in snapshot_numbers:
+    snapdir = base_path / f"snapdir_{snap:03d}"
+    chunks = []
+
+    for i in range(n_files_per_snapshot):
+        file = snapdir / f"snapshot_{snap:03d}.{i}.hdf5"
+        if not file.exists():
+            continue
 
     with h5py.File(file, "r") as f:
         positions.append(f["PartType1/Coordinates"][:])
