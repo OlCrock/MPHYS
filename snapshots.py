aabb649390105bb44bbe40e98584547ed8e64dd3
Oliver Crocker <s2521629@ed.ac.uk>
2026-09-23 17:04:04 +0100

    removing pandas as not in cuillin

--- snapshots.py
+++ snapshots.py
@@ -1,8 +1,6 @@
-import numpy as np
-import pandas as pd
 import yt 
 
-
+#snapshots
 ds = yt.load("/disk12/legacy/GVD_C700_l100n256_SLEGAC/dm_gadget/data/snapdir_005/snapshot_005.0.hdf5")
 
 print(ds)
