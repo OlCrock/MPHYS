18772fb89c39761d9f4b807be4dd9b8224150d93
Oliver Crocker <s2521629@ed.ac.uk>
2026-09-23 17:27:39 +0100

    retreiving positions of particles from snapshot 5

--- snapshots.py
+++ snapshots.py
@@ -1,9 +1,24 @@
-import yt 
+import h5py
+import numpy as np
 
-#snapshots
-ds = yt.load("/disk12/legacy/GVD_C700_l100n256_SLEGAC/dm_gadget/data/snapdir_005/snapshot_005.0.hdf5")
+path = "/disk12/legacy/GVD_C700_l100n256_SLEGAC/dm_gadget/data/snapdir_005"
 
-print(ds)
+positions = []
+
+for i in range(4):
+    file = f"{path}/snapshot_005.{i}.hdf5"
+
+    with h5py.File(file, "r") as f:
+        positions.append(f["PartType1/Coordinates"][:])
+
+positions = np.concatenate(positions)
+
+x = positions[:, 0]
+y = positions[:, 1]
+
+print("Number of particles:", len(x))
+print("x shape:", x.shape)
+print("y shape:", y.shape)
 
 
 
